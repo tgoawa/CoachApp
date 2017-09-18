27ae565a903cdc807a04f9bfcb224bcc67683486
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-09-18 10:07:08 -0500

    created login module and component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Cookie } from 'ng2-cookies';
+import * as CryptoJS from 'crypto-js';
+import { LoginService } from './services/login.service';
+import { LoggerService } from '../core/services/logger.service';
+import { User } from './user';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private lgService: LoginService,
+    private logger: LoggerService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.loginForm = this.toFormGroup();
+  }
+
+  private checkUser(user: User) {
+    this.lgService.checkStatus(user)
+    .subscribe(data => {
+      this.logger.log('Login Successful!');
+      this.setAuthStatus(data);
+    }, error => {
+      this.logger.error(error);
+    });
+  }
+
+  private encryptUser() {
+    const key = CryptoJS.enc.Utf8.parse('8080808080808080');
+    const iv = CryptoJS.enc.Utf8.parse('8080808080808080');
+    const encryptedUser: User = {
+    username: '',
+    password: ''
+    };
+
+    encryptedUser.username = CryptoJS.AES.encrypt(this.loginForm.get('username').value, key, {iv: iv}).toString();
+    encryptedUser.password = CryptoJS.AES.encrypt(this.loginForm.get('password').value, key, {iv: iv}).toString();
+    return encryptedUser;
+  }
+
+  private setAuthStatus(data: boolean) {
+    if (data) {
+      Cookie.set('user', this.loginForm.get('username').value, 45);
+      this.router.navigate(['/home']);
+    } else {
+      this.logger.error('Login Failed!');
+      console.log('Error logging in');
+    }
+  }
+
+  private toFormGroup() {
+    const formGroup = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+
+    return formGroup;
+  }
+
+}
