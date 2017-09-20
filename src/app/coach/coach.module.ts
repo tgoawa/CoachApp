edb3dfa1c3ad526683d91f71eabc047bd9ee55ff
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-09-20 12:48:00 -0500

    generated coach feature module

---
+++ src/app/coach/coach.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoachRoutingModule } from './coach-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    CoachRoutingModule
+  ],
+  declarations: []
+})
+export class CoachModule { }
