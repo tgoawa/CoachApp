72928e58a6c17a3905da7560e70796fe104b8c82
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-09-20 08:26:20 -0500

    working on team member object

--- src/app/main/landing-page/landing-page.component.ts
+++ src/app/main/landing-page/landing-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TeamMember } from '../../core/teamMember/team-member';
 import { TeamMemberService } from '../../core/teamMember/team-member.service';
 import { ActivatedRoute } from '@angular/router';
+import { Cookie } from 'ng2-cookies';
 
 @Component({
   selector: 'app-landing-page',
@@ -10,14 +11,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class LandingPageComponent implements OnInit {
   teamMember: TeamMember;
+  userName: string;
 
   constructor(private activatedRoute: ActivatedRoute,
     private tmService: TeamMemberService) { }
 
   ngOnInit() {
-    this.teamMember = this.activatedRoute.snapshot.data['teamMemberData'];
+    // this.teamMember = this.activatedRoute.snapshot.data['teamMemberData'];
+    // console.log(this.activatedRoute.snapshot.data['teamMemberData']);
+    // this.teamMember = this.tmService.getTeamMember(Cookie.get('user'));
     this.tmService.defaultTeamMember = this.teamMember;
-    this.tmService.setEmulatedTeamMember(this.teamMember);
   }
 
 }
