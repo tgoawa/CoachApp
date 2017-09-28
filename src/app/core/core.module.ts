0fcf408a6eaf9cd56c07ec10d3b918271b1e66c2
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-09-28 11:35:43 -0500

    added logged in info to toolbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-import { MdToolbarModule, MdButtonModule } from '@angular/material';
+import { MdToolbarModule, MdButtonModule, MdIconModule } from '@angular/material';
 
 import { throwIfAlreadyLoaded } from './module-import.guard';
 import { HeaderComponent } from './header/header.component';
@@ -18,7 +18,8 @@ import { TeamMemberService } from './teamMember/team-member.service';
     HttpModule,
     RouterModule,
     MdToolbarModule,
-    MdButtonModule
+    MdButtonModule,
+    MdIconModule
   ],
   exports: [
     HeaderComponent,
