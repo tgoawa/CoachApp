6f942593017a5f2a52552b4eb4719fce676e9d04
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-10-02 08:42:54 -0500

    added logout functionality to header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TeamMemberService } from '../teamMember/team-member.service';
 import { TeamMember } from '../teamMember/team-member';
 import { Observable } from 'rxjs/Observable';
+
+import { Cookie } from 'ng2-cookies';
 
 @Component({
   selector: 'app-header',
@@ -12,7 +15,7 @@ export class HeaderComponent implements OnInit {
   defaultTeamMember$: Observable<TeamMember>;
   emulatedTeamMember$:  Observable<TeamMember>;
 
-  constructor(private tmService: TeamMemberService) { }
+  constructor(private tmService: TeamMemberService, private router: Router) { }
 
   ngOnInit() {
     this.defaultTeamMember$ = this.tmService.defaultTeamMember$;
@@ -21,6 +24,11 @@ export class HeaderComponent implements OnInit {
 
   resetToDefaultTeamMember() {
     this.tmService.resetEmulatedTeamMember();
+  }
+
+  onLogout() {
+    Cookie.delete('user');
+    this.router.navigate(['/login']);
   }
 
 }
