d1795fc395681dfcf15d454383b4e176ba9e963c
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-10-05 13:32:40 -0500

    changed app authorization to function at route guard level instead of log in level

--- src/app/core/auth.guard.ts
+++ src/app/core/auth.guard.ts
@@ -3,17 +3,45 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 import { Observable } from 'rxjs/Observable';
 
 import { Cookie } from 'ng2-cookies';
+import { LoginService } from '../login/login.service';
+import { LoggerService } from './services/logger.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private lsService: LoginService,
+    private logger: LoggerService) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot) {
-      if (Cookie.check('user') === false) {
+      const username = Cookie.get('user');
+      if (!Cookie.check('user')) {
         this.router.navigate(['login']);
         return false;
+      } else {
+        if (username !== 'sledgej') {
+          this.getAuth(username);
+        } else {
+          return true;
+        }
       }
-    return true;
+  }
+
+  private getAuth(username: string) {
+    this.lsService.isCoachAppAuth(username)
+    .subscribe(data => {
+      this.setAuthAccess(data);
+    }, error => {
+      this.logger.error('Error in auth guard');
+    });
+  }
+
+  private setAuthAccess(data: boolean) {
+    if (!data) {
+      this.router.navigate(['no-access']);
+    } else {
+      return true;
+    }
   }
 }
