1597767a8573af06867998535919f83a52cf5c8f
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-10-06 08:20:38 -0500

    some code clean up

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -6,7 +6,13 @@ import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
-import { MatCardModule, MatAutocompleteModule, MatInputModule, MatButtonModule, MatSnackBarModule } from '@angular/material';
+import {
+  MatCardModule,
+  MatAutocompleteModule,
+  MatInputModule,
+  MatButtonModule,
+  MatSnackBarModule,
+} from '@angular/material';
 
 @NgModule({
   imports: [
@@ -18,7 +24,7 @@ import { MatCardModule, MatAutocompleteModule, MatInputModule, MatButtonModule, 
     MatAutocompleteModule,
     MatInputModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
   ],
   declarations: [
     MainComponent,
